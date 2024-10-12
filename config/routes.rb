bc483c1f02916a47dd060a6cc87218491d81ce51
sugielff <sugielff@gmail.com>
2024-10-12 22:14:07 +0900

    Add article CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get "articles/index"
   root "articles#index"
 
-  get "/articles", to: "articles#index"
+  resources :articles 
+    resources :comments
+  end
 
-  get "article/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
